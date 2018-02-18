dd6e614ea9d6591e364bd4ca73e32c2a48fe81c6
Brad <brad.anderson58@gmail.com>
2018-02-18 15:44:54 -0500

    add pymysql and token interceptor fix

--- client/src/app/services/token.interceptor.ts
+++ client/src/app/services/token.interceptor.ts
@@ -12,12 +12,13 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor(public auth: AuthenticationService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Token ${this.auth.getAuth()}`
-      }
-    });
-    console.log(request);
+    if (!request.url.includes('get_auth_token')) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Token ${this.auth.getAuth()}`
+        }
+      });
+    }
     return next.handle(request);
   }
 }
