a197497508f6a7ce4eed7fe8abd0c0d7c8742ee4
Brad <brad.anderson58@gmail.com>
2018-02-22 20:11:21 -0500

    csv validation for file uploads

--- server/uploads/views.py
+++ server/uploads/views.py
@@ -4,13 +4,19 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+from uploads.csvValidator import CsvValidator
 
 class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
+    csvValidator = CsvValidator()  # TODO: dependency injection in python?
 
     def post(self, request, format=None):
-        print(request.data)
         file_obj = request.data['fileKey']
-        print(file_obj.name)
-        path = default_storage.save('data_files/' + file_obj.name, ContentFile(file_obj.read())) # TODO: write in chunks for large data sets
+
+        # validate csv file
+        if self.csvValidator.invalidUpload(file_obj.file):
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        file_obj.file.seek(0)
+        path = default_storage.save('data_files/' + file_obj.name, ContentFile(file_obj.read())) # TODO: write in chunks for large data sets / compress / timestamp
         return Response(status=status.HTTP_204_NO_CONTENT)
